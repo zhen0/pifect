4add275ccf3472096b4b01556a1d53aba5a3fe91
Daniel P. Egan <dp.egan@gmail.com>
2019-11-03 16:41:22 -0500

    simplify

--- src/git_pull_master.py
+++ src/git_pull_master.py
@@ -1,15 +1,2 @@
-from prefect import task, Flow
-from datetime import timedelta
-from prefect.schedules import IntervalSchedule
 import os
-
-@task
-def update_local_branch():
-    os.system("git pull origin master")
-
-schedule = IntervalSchedule(interval=timedelta(seconds=60))
-
-with Flow("Pull git master", schedule) as flow:
-    update_local_branch()
-
-flow.run()
+os.system("git pull origin master")
