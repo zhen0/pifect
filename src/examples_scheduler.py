e906ad38b55f429dea1d37b95b2868d9cb38ef86
Daniel P. Egan <dp.egan@gmail.com>
2019-11-03 16:28:04 -0500

    message fancy

--- src/examples_scheduler.py
+++ src/examples_scheduler.py
@@ -4,19 +4,20 @@ from prefect.schedules import IntervalSchedule
 # Pushbullet API
 from pushbullet import Pushbullet
 import yaml
+import datetime
 
 with open('credentials.yaml') as f:
     credentials = yaml.safe_load(f)
 
 @task
-def push_hello(user_id):
+def push_hello(user_id, message):
     pb = Pushbullet(credentials[user_id]["pushbullet_key"])
-    push = pb.push_note("Hey " + user_id + "!", "This is a message for you.")
+    push = pb.push_note("Hey " + user_id + "!", message)
 
 schedule = IntervalSchedule(interval=timedelta(seconds=60))
 
 with Flow("Hello", schedule) as flow:
-    push_hello("Dan")
+    push_hello("Dan", "The time here is " + datetime.datetime.now())
 
 flow.run()
 
