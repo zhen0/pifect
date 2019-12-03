397db1f87df607dca45ecf9b210f4fe2465191ec
Daniel P. Egan <dp.egan@gmail.com>
2019-12-03 09:08:23 -0500

    main vs components

--- src/tweetbot/tweetbot_main.py
+++ src/tweetbot/tweetbot_main.py
@@ -1,118 +1,28 @@
-# @author Daniel Egan
-# Twitter bot auto-posting content.
-# Uses Prefect, GoogleSheets, Twitter, and Pushbullet APIs.
-# Use at your own risk.
+import tweet_flow_components as tfc
 import os
-import pandas as pd
-import yaml
-from datetime import date, timedelta
-# Use google sheets as easy-access database
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
-# Twitter
-import twitter
-# Prefect
-from prefect import task, Flow, Task
-from prefect.schedules import IntervalSchedule
-# Pushbullet
 from pushbullet import Pushbullet
+from prefect import task, Flow, Task, Parameter
+from prefect.schedules import IntervalSchedule
 
+credentials_file = os.path.join('/', 'Users', 'danielegan', 'src','degan_creds.yaml')
+creds = tfc.load_creds(credentials_file)
+pb = Pushbullet(creds['pushbullet']['token'])
 
-@task(name= "load_creds", slug = "load_creds")
-def load_creds(local_yaml_file):
-    creds = yaml.safe_load(open(local_yaml_file, 'r'))
-    return creds
-
-@task(name= "twitter_auth", slug = "twitter_auth")
-def twitterAuth(creds):
-    twitter_creds = creds['twitter']
-    api = twitter.Api(consumer_key        = twitter_creds['consumer_key'],
-                      consumer_secret     = twitter_creds['consumer_secret'],
-                      access_token_key    = twitter_creds['access_token'],
-                      access_token_secret = twitter_creds['access_token_secret'])
-
-
-
-# Autheticate
-@task(name= "gsheets_auth", slug = "gsheets_auth")
-def gsheetsAuth():
-    kLocalPath = os.path.join('/','Users','danielegan','src','pifect','src', 'tweetbot')
-    scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
-    creds = ServiceAccountCredentials.from_json_keyfile_name(os.path.join(kLocalPath, 'client_secrets.json'), scope)
-    client = gspread.authorize(creds)
-    return client
-
-
-@task(name="getsheet", slug="getsheet")
-def getSheet(spreadSheet, workSheet):
-    sheet = client.open(spreadSheet).worksheet(workSheet)
-    return sheet
-
-@task(name="convertSheetToPD", slug="convertSheetToPD")
-def convertSheetToPD(workSheet):
-    posts = pd.DataFrame(workSheet.get_all_values())
-    new_header = posts.iloc[0] #grab the first row for the header
-    posts = posts[1:] #take the data less the header row
-    posts.columns = new_header #set the header row as the df header
-    return posts
-
-@task(name="getNewPostIndex", slug="getNewPostIndex")
-def getNewPostIndex(posts):
-    posts['LastPostedDT'] = pd.to_datetime(posts['LastPosted'])
-    post_index = posts['LastPostedDT'].idxmin() # Get the index of the oldest row
-    return post_index
-
-@task(name="getNewPost", slug="getNewPost")
-def getNewPost(posts, post_index):
-    new_post = posts.loc[post_index].Post # Get the post
-    return new_post
-
-@task(name="sendTweet", slug="sendTweet")
-def sendTweet(new_post):
-    if (len(new_post) < 20):
-        status = "post too short. must be error"
-        return status
-    else:
-        status = api.PostUpdate(new_post)
-    return status.text
-
-# @task (name="handler", slug="handler")
-def sendPushBulletUpdate(task, old_state, new_state):
-    if new_state.is_finished():
-        msg = "Task {0} finished in state {1}".format(task, new_state)
-    pb = Pushbullet()
-    pb.push_note('Flow', msg)
-    return new_state
-
-@task(name="updatePostQueue", slug="updatePostQueue", state_handlers=[sendPushBulletUpdate])
-def updatePostQueue(workSheet, post_index):
-    today = date.today()
-    workSheet.update_cell(post_index, 3, today.strftime("%Y/%m/%d"))
-
-
-# @task(name="sendPushBulletUpdate", slug="sendPushBulletUpdate")
-# def sendPushBulletUpdate(title, message):
-#     status = pb.push_note(title, message)
-#     return status
-
-# @task
-
-
-
-credentials_file = os.path.join('credentials.yaml')
-t = Task(state_handlers=[sendPushBulletUpdate])
-with Flow(name="Run BeFiWins", tasks=[t]) as flow:
-    creds               = load_creds(credentials_file)
-    # pb_auth_res         = pushbullAuth(creds)
-    tw_auth_res         = twitterAuth(credentials_file)
-    gs_auth_res         = gsheetsAuth()
-    sheet               = getSheet("Twitter Posts", "BeFiWins")
-    posts               = convertSheetToPD(sheet)
-    post_index          = getNewPostIndex(posts)
-    new_post            = getNewPost(posts, post_index)
-    send_result         = sendTweet(new_post)
-    update_sheet_result = updatePostQueue(sheet, post_index)
-    update_message = flow.name + " ran: "
-    # send_PB_update      = sendPushBulletUpdate()
+#BeFiWins
+t = Task(state_handlers=[tfc.sendPushBulletOnFail])
+with Flow(name="BeFiWins", tasks=[t]) as flow:
+    spreadSheet = "Twitter Posts"
+    workSheet = "BeFiWins"
+    send_PB_update      = tfc.sendPushBulletUpdate(flow.name + " is running ", 'hi')
+    twitter_api         = tfc.twitterAuth(creds)
+    gs_client           = tfc.gsheetsAuth()
+    sheet               = tfc.getSheet(gs_client, spreadSheet, workSheet)
+    posts               = tfc.convertSheetToPD(sheet)
+    post_index          = tfc.getNewPostIndex(posts)
+    new_post            = tfc.getNewPost(posts, post_index)
+    final_post          = tfc.modifyTweet(new_post, workSheet)
+    send_result         = tfc.sendTweet(final_post, twitter_api)
+    update_sheet_result = tfc.updatePostQueue(sheet, post_index)
+    send_PB_update      = tfc.sendPushBulletUpdate(flow.name + " ran: ", send_result)
 
 flow.run()
